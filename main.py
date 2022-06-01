359f52db2f94ddded6635c89ee635206f38352bc
Grace Kang <106709573+GraceKNG@users.noreply.github.com>
2022-06-01 22:31:17 +0000

    debugged code so it works. Added score=0 under global score

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ class Quiz:
            
             
             #radio button5
-            self.confirm_button = Button(self.quiz_frame, text="Confirm", bg="white")
+            self.confirm_button = Button(self.quiz_frame, text="Confirm", bg="white", command=self.test_progress)
             self.confirm_button.grid(row=5)
                       
             self.score_label = Label(self.quiz_frame, text="SCORE", font=("Tw Cen Mt","16"),bg=background_color,)
@@ -93,46 +93,41 @@ class Quiz:
        randomiser()
        self.var1.set(0)
        self.question_label.config(text=questions_answers[qnum][0])
-       self.rb1.config(text=quesions_answers[qnum][1])
-       self.rb2.config(text=quesions_answers[qnum][2])
-       self.rb3.config(text=quesions_answers[qnum][3])
-       self.rb4.config(text=quesions_answers[qnum][4])
+       self.rb1.config(text=questions_answers[qnum][1])
+       self.rb2.config(text=questions_answers[qnum][2])
+       self.rb3.config(text=questions_answers[qnum][3])
+       self.rb4.config(text=questions_answers[qnum][4])
 
     def test_progress(self):
-        global score
-        scre_label = self.score_label
-        choice = self.var1.get()
-        if len(asked)>9: 
-          if choice == questions_answers[qnum][6]:
-            score +=1
+      global score
+      score =0 
+      scr_label = self.score_label
+      choice = self.var1.get()
+      if len(asked)>4: 
+        if choice == questions_answers[qnum][6]:
+          score +=1
+          scr_label.configure(text=score)
+          self.confirm_button.config(text="Confirm")
+        else:
+          score+=0
+          scr_label.configure(text="The correct answer was " + questions_answers[qnum][5])
+          self.confirm_button.config(text="Confirm")
+      else:
+        if choice==0:
+          self.confirm_button.config(text="Try again please,you did not select anything")
+          choice-self.var1.get()
+        else:
+          if choice==questions_answers[qnum][6]:
+            score+=1
             scr_label.configure(text=score)
             self.confirm_button.config(text="Confirm")
+            self.questions_setup()
           else:
             score+=0
             scr_label.configure(text="The correct answer was " + questions_answers[qnum][5])
-            self.confirm_button.config(text="Confirm")
-        else:
-          if choice==0:
-            self.confirm_button.config(text="Try again please,you did not select anything")
-            choice-self.var1.get()
-          else:
-            if choice==questions_answers[qnum][6]:
-              score+=1
-              scr_label.configure(text=SCORE)
-              self.confirm_button(text="Confirm")
-              self.questions_setup()
-            else:
-              score+=0
-              scr_label.configure(text="The correct answer was " + questions_answers[qnum][5])
-              self.confirm_button.configure(text="Confirm")
-              self.questions_setup()
-
-      
-
-      
-   
-
-                        
+            self.confirm_button.configure(text="Confirm")
+            self.questions_setup()
+        
                     
 randomiser()     
 if __name__ == "__main__":
